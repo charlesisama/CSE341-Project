699fe5a25abc260d7e6c3b3912e73f95357dd1a5
charlesisama <cisama@byupathway.edu>
2026-09-09 03:07:28 +0100

    feat: implement routing by adding routes module and remove inline hello world response

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello, World! Welcome to my Express server.\r\n');
-});
+app.use('/', require('./routes'));
+
 
 const port = process.env.PORT || 3000;
 
