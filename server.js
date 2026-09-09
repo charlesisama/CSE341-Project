3976386730ba3f17ca0f608e34ec160527968340
charlesisama <cisama@byupathway.edu>
2026-09-09 06:06:43 +0100

    feat: refactor MongoDB connection handling and update environment variable name

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
 
+const mongodb = require('./routes/data/database');
+
 app.use('/', require('./routes'));
 
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log('Server is running on port ' + port);
+mongodb.initDb(err => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => {
+            console.log('Server is running on port ' + port);
+        });
+
+    }
 });
+
