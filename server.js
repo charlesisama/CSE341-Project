134a8188941919c5aab65f39d92e8335a3f68356
charlesisama <cisama@byupathway.edu>
2026-09-09 03:04:08 +0100

    feat: use environment variable for server port configuration

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ app.get('/', (req, res) => {
     res.send('Hello, World! Welcome to my Express server.\r\n');
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(process.env.port || port, () => {
-    console.log('Server is running on port ' + (process.env.PORT || port));
+app.listen(port, () => {
+    console.log('Server is running on port ' + port);
 });
