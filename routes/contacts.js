2fa06735d62b6f073a7b5a5b26b3255a25ada5e3
charlesisama <cisama@byupathway.edu>
2026-09-10 12:58:33 +0100

    feat: remove unused MongoDB import from contacts route

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
 
-const mongodb = require('../data/contactdb');
-
 const contactsController = require('../controllers/contacts');
 
 router.get('/contacts', contactsController.getAllContacts);
