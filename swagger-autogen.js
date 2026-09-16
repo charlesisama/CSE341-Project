a15db1d06bbeebe789625ade62544ed9c42cda18
charlesisama <cisama@byupathway.edu>
2026-09-16 23:12:32 +0100

    feat: update Swagger documentation with correct host and enhanced description

--- swagger-autogen.js
+++ swagger-autogen.js
@@ -1,16 +1,23 @@
 const swaggerAutogen = require("swagger-autogen")();
 
-// Define the Swagger document
 const doc = {
     info: {
         title: "Contacts API",
-        description: "CSE 341 Contacts API",
+        description: "CSE 341 Contacts API - Contacts Part 2",
+        version: "1.0.0"
     },
-    host: "localhost:3000",
+    host: "cse341-t68c.onrender.com",
+    schemes: ["https"],
+    basePath: "/",
+    tags: [
+        {
+            name: "Contacts",
+            description: "Operations for managing contacts"
+        }
+    ]
 };
 
 const outputFile = "./swagger.json";
 const endpointsFiles = ["./routes/index.js"];
 
-// Generate the Swagger documentation
 swaggerAutogen(outputFile, endpointsFiles, doc);
