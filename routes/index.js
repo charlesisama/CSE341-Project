ddaab49cef8a1923c420b9353408bd6307fcbba6
charlesisama <cisama@byupathway.edu>
2026-09-17 13:29:08 +0100

    feat: add greeting endpoint for Contacts API

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
 
+router.get("/", (req, res) => {
+    res.send("Hello World, this is the Contacts API!");
+});
 router.use("/contacts", require("./contacts"));
 router.use("/api-docs", require("./swagger"));
 
