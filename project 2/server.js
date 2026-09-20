6988856d98fa9cd84b71cbb61b0ff605aaec3990
charlesisama <cisama@byupathway.edu>
2026-09-20 22:31:54 +0100

    feat: add Swagger UI setup and API documentation for student and course management

--- project 2/server.js
+++ project 2/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+
 const database = require("./data/database");
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/", require("./routes"));
 
