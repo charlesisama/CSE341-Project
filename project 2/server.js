65c99ef393f527772e3e4d2cfae890184184d80d
charlesisama <cisama@byupathway.edu>
2026-09-26 12:54:18 +0100

    feat: add Passport.js and GitHub authentication with session management

--- project 2/server.js
+++ project 2/server.js
@@ -1,5 +1,47 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+const passport = require("passport");
+const session = require("express-session");
+const GitHubStrategy = require("passport-github2").Strategy;
+
 const app = express();
+
+// Load environment variables from .env file
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true
+    })
+);
+
+// Initialize Passport and session middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Serialize and deserialize user for session management
+passport.use(
+    new GitHubStrategy(
+        {
+            clientID: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            callbackURL: process.env.CALLBACK_URL
+        },
+        function (accessToken, refreshToken, profile, done) {
+            return done(null, profile);
+        }
+    )
+);
+
+// Serialize and deserialize user for session management
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
 
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
